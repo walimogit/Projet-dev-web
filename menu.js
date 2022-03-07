4f5a6773e12d91a5fd89f44c3369f015473c6116
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-03-07 21:51:06 +0100

    JS Menu collant

--- menu.js
+++ menu.js
@@ -17,21 +17,41 @@ window.onclick = function(e) {
 
 //----Menu-collant----
 
-let scrollY = function() {
-  var supportPageOffset = window.pageXOffset !== undefined;
-  var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-  return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
-}
+// Fonctions
 
+let scrollY = window.scrollY
 let element = document.querySelector('.topnav')
-let topbar = document.querySelector('.topnav').getBoundingClientRect().top + window.scrollY
+let rect = element.getBoundingClientRect()
+let topbar = rect.top + scrollY
+//let fake = document.createElement('div')
+//fake.style.width = rect.width + "px"
+//fake.style.height = rect.height + "px"
 console.log(topbar)
 let onScroll = function () {
-  
-  /*if (element.getBoundingClientRect().top < 0) {
+  let hasScrollClass = element.classList.contains('fixed')
+  if (window.scrollY > topbar && !hasScrollClass){
     element.classList.add('fixed')
-  } else {
+    element.style.width = rect.width + "px"
+    //element.parentNode.insertBefore(fake, element)
+  } else if (window.scrollY < top && hasScrollClass) {
     element.classList.remove('fixed')
-  }*/
+    //element.parentNode.remove(fake, element)
+  }
 }
+
+let onResize = function () {
+  element.style.width = "auto"
+  element.classList.remove('fixed')
+  //fake.style.display = "none"
+  rect = element.getBoundingClientRect()
+  topbar = rect.top + window.scrollY
+  //fake.style.width = rect.width + "px"
+  //fake.style.height = rect.height + "px"
+  //fake.style.display = "block"
+  onScroll()
+}
+
+// Listener
+
 window.addEventListener('scroll', onScroll)
+window.addEventListener('resize', onResize)
