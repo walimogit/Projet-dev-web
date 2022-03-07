a0e988680337aaf9d9c4516a3d38eb97dd63114c
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-03-07 16:05:42 +0100

    modif menu collant V1

--- menu.js
+++ menu.js
@@ -1,3 +1,6 @@
+
+
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -7,10 +10,31 @@ window.onclick = function(e) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content");
     for (var d = 0; d < dropdowns.length; d++) {
-      var openDropdown = dropdowns[d];
+      let openDropdown = dropdowns[d];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
   }
 }
+
+//----Menu-collant----
+
+let scrollY = function() {
+  var supportPageOffset = window.pageXOffset !== undefined;
+  var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+  var x = supportPageOffset ? window.pageXOffset : isCSS1Compat ? document.documentElement.scrollLeft : document.body.scrollLeft;
+  var y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+}
+
+let element = document.querySelector('.topnav')
+let onScroll = function () {
+  
+  if (element.getBoundingClientRect().top < 0) {
+    element.classList.add('fixed')
+  } else {
+    element.classList.remove('fixed')
+  }
+}
+window.addEventListener('scroll', onScroll)
