131ceae4b46fd7f0e018d322780bde2402d6c8f3
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-03-17 17:47:22 +0100

    Déplacement code JS dans fichier JS dédié register

--- menu.js
+++ menu.js
@@ -15,24 +15,6 @@ window.onclick = function(e) {
   }
 }
 
-
-
-
-var btnCreateAccount = document.querySelector('input[id="create_account"]');
-var cgvCheckbox = document.querySelector('input[id="conditions"]');
-btnCreateAccount.disabled = true
-console.log('true')
-cgvCheckbox.click = function() {
-  if(cgvCheckbox.checked) {
-    btnCreateAccount.disabled = false
-    console.log('false')
-  }
-  else {
-    btnCreateAccount.disabled = true
-    console.log('true')
-  }
-}
-
 //----Menu-collant----
 
 // Fonctions
