91413b22e6f4b55f77ca576164ed6857dd20acf3
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-03-21 18:20:16 +0100

    Changement nom bdd à interroger

--- www/app/bdd.php
+++ www/app/bdd.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dsn = 'mysql:dbname=bddprojet;host=db;port=3306;charset=UTF8'; //a setup
+    $dsn = 'mysql:dbname=bddprojetweb;host=db;port=3306;charset=UTF8'; //a setup
     $user = 'root';
     $password = 'cesi';
     $bdd = new PDO($dsn, $user, $password);
