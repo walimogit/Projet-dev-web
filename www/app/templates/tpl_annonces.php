878bda7ffe9eae2c46484920f81286b448f21894
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-03-24 00:30:53 +0100

    création carrousel dans annonces

--- www/app/templates/tpl_annonces.php
+++ www/app/templates/tpl_annonces.php
@@ -1,4 +1,49 @@
-<?php foreach($annonces as $annonce): ?>
+<main class="main">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <fieldset>
+                    <h5>Nouvelles annonces</h5>
+                </fieldset>
+            </div>
+            <?php foreach($annonces as $annonce): ?>
+            <div class="carousel-item annonce">
+                <fieldset>
+                    <h5 class="col">
+                        Annonce <?= $annonce ?>
+                    </h5>
+                    <div class="col">
+                        Competense <?= $annonce ?>
+                    </div>
+                    <div class="col">
+                        Duree_stage <?= $annonce ?>
+                    </div>
+                    <div class="col">
+                        Base_remuneration <?= $annonce ?>
+                    </div>
+                    <div class="col">
+                        Date_offre <?= $annonce ?>
+                    </div>
+                    <div class="col">
+                        Nb_places_offertes <?= $annonce ?>
+                    </div>
+                </fieldset>
+            </div>
+            <?php endforeach; ?>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</main>
+
+
+<!--<?php //foreach($annonces as $annonce): ?>
 
 <div class="row">
     <div class="col-auto">
@@ -14,9 +59,8 @@
             </div>
         </div>
     </div>
-</div>
+</div>-->
 
-<?php endforeach; ?>
 
 <!--liste d'annonce-->
 
--- www/app/templates/tpl_footer.php
+++ www/app/templates/tpl_footer.php
@@ -1,5 +1,6 @@
 </div>
         <script src="../js/menu.js"></script>
+        <script src="../vendors/bootstrap/js/bootstrap.js"></script>
         <!--<script src="script.js"></script>-->
     </body>
 </html>
