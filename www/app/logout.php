8759a0db5719f5715ab51c857cdeddd0d622a2cc
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-03-23 19:28:25 +0100

    change location into logout

--- www/app/logout.php
+++ www/app/logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 session_destroy();
-if(empty($_SESSION['sess_user_id'])) header("location: index.html");
+if(empty($_SESSION['sess_user_id'])) header("location: index.php");
 ?>
