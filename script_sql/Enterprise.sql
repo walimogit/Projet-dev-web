9349b747e4b9f7278b5fb05d075f8519393c3730
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-03-23 12:45:08 +0100

    Update Enterprise.sql

--- script_sql/Enterprise.sql
+++ script_sql/Enterprise.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : lun. 21 mars 2022 à 15:21
+-- Généré le : mer. 23 mars 2022 à 11:28
 -- Version du serveur : 10.3.34-MariaDB-1:10.3.34+maria~focal
 -- Version de PHP : 8.0.16
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `bddprojet`
+-- Base de données : `bddprojetweb`
 --
 
 -- --------------------------------------------------------
@@ -31,6 +31,8 @@ CREATE TABLE `Enterprise` (
   `ID_enterprise` int(11) NOT NULL,
   `Name_enterprise` text NOT NULL,
   `Number_interns_accepted` text NOT NULL,
+  `Login` text NOT NULL,
+  `Password` text NOT NULL,
   `Booldel` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -38,22 +40,22 @@ CREATE TABLE `Enterprise` (
 -- Déchargement des données de la table `Enterprise`
 --
 
-INSERT INTO `Enterprise` (`ID_enterprise`, `Name_enterprise`, `Number_interns_accepted`, `Booldel`) VALUES
-(1, 'PWC', '3', 1),
-(2, 'Onet', '3', 1),
-(3, 'Auchan', '3', 1),
-(4, 'Carrefour', '2', 1),
-(5, 'Leclerc', '1', 1),
-(6, 'Boulanger', '2', 1),
-(7, 'Darty', '2', 1),
-(8, 'Cactus', '2', 1),
-(9, 'CGT', '1', 1),
-(10, 'BNP Paribas', '1', 1),
-(11, 'Alstom', '2', 1),
-(12, 'Citroën', '3', 1),
-(13, 'Peugeot', '2', 1),
-(14, 'Inter-Actif', '3', 1),
-(15, 'Noneo', '1', 1);
+INSERT INTO `Enterprise` (`ID_enterprise`, `Name_enterprise`, `Number_interns_accepted`, `Login`, `Password`, `Booldel`) VALUES
+(1, 'PWC', '3', 'PWC1', 'KQYWO', 1),
+(2, 'Onet', '3', 'Onet2', 'QPQKH', 1),
+(3, 'Auchan', '3', 'Auchan3', 'XLBVE', 1),
+(4, 'Carrefour', '2', 'Carrefour4', 'CYMWG', 1),
+(5, 'Leclerc', '1', 'Leclerc5', 'ZYAHJ', 1),
+(6, 'Boulanger', '2', 'Boulanger6', 'PGDRM', 1),
+(7, 'Darty', '2', 'Darty7', 'TDINV', 1),
+(8, 'Cactus', '2', 'Cactus8', 'OENQP', 1),
+(9, 'CGT', '1', 'CGT9', 'GLIUK', 1),
+(10, 'BNP Paribas', '1', 'BNP Paribas10', 'YFNKI', 1),
+(11, 'Alstom', '2', 'Alstom11', 'TLIQG', 1),
+(12, 'Citroën', '3', 'Citroën12', 'CWSBS', 1),
+(13, 'Peugeot', '2', 'Peugeot13', 'TPPHR', 1),
+(14, 'Inter-Actif', '3', 'Inter-Actif14', 'KPFPI', 1),
+(15, 'Noneo', '1', 'Noneo15', 'UNSLK', 1);
 
 --
 -- Index pour les tables déchargées
