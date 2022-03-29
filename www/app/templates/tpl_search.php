7232f92ddd7d4db027b8b61106c3f275db2f1c30
Maxougit <02maxou@gmail.com>
2022-03-29 09:03:57 +0200

    Revert "add <pre>"
    This reverts commit 8918e54c0a33a5c1cd4ff8c7085c3c0b370a49c9.

--- www/app/templates/tpl_search.php
+++ www/app/templates/tpl_search.php
@@ -3,7 +3,7 @@
         <div class="col">
             <label>Search</label>
             <input type="text" class="form-control" name="nom" value="<?php echo $search ?>" />
-            <pre><?php print_r ($result_stage) ?></pre>
+            <?php print_r ($result_stage) ?>
         </div>
     </div>
 </form>
