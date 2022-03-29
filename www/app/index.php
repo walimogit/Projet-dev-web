8f7415fdc81d4f57868e41899e1cf08f7da9d636
Maxougit <02maxou@gmail.com>
2022-03-29 09:20:38 +0200

    clean time

--- www/app/index.php
+++ www/app/index.php
@@ -5,16 +5,10 @@ require_once("tools/tools.php");
 
 if(!isLoggedIn())
     header('Location: login.php');
-    
-//TODO logique bdd / $^_POST etc...
-
 
 $result_stage = Search();
 
-require_once('templates/tpl_header.php');
-
-var_dump(isStudent());
-
+require_once('templates/tpl_header.php');   
 require_once('templates/tpl_index.php');
 require_once('templates/tpl_footer.php');
 
