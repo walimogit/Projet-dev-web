afa7f08babfb9a15b0d04a9fedddb46880b13bb2
Blotzeen <74916752+Blotzeen@users.noreply.github.com>
2022-03-29 14:48:27 +0200

    better footer

--- www/app/templates/tpl_footer.php
+++ www/app/templates/tpl_footer.php
@@ -1,20 +1,19 @@
 <link rel="stylesheet" href="../css/navbar-top-fixed.css">
 </div>
-
-<div class="footer pb-2 bg-dark">
+</div>
+<div class="Footer pb-2 bg-dark">
     <div class="container">
         <div class="row mb-3">
-            <div class="col">
+            <div class="col-sm-7 col-lg-5">
                 <h4>Nous</h4>
                 <ul class="u-listUnstyled">
-                    <li>LEROUX MAXENCE <a href="mailto:maxence.leroux@viacesi.fr"> maxence.leroux@viacesi.fr</a></li>
-                    <li>ROGE JULES<a href="mailto:jules.roge@viacesi.fr"> jules.roge@viacesi.fr</a></li>
-                    <li>RENAUD TIMOTHEE<a href="mailto:timothee.renaud1@viacesi.fr"> timothee.renaud1@viacesi.fr</a>
-                    </li>
+                    <li><a href="mailto:maxence.leroux@viacesi.fr">LEROUX MAXENCE</a></li>
+                    <li><a href="mailto:jules.roge@viacesi.fr">ROGE JULES</a></li>
+                    <li><a href="mailto:timothee.renaud1@viacesi.fr">RENAUD TIMOTHEE</a></li>
                 </ul>
             </div>
             <div class="col-sm-1 col-lg-1"></div>
-            <div class="col">
+            <div class="col-sm-7 col-lg-5">
                 <h4>Comm</h4>
                 <ul class="u-listUnstyled">
                     <li><a href="" class="text-decoration-none text-muted">Notre Discord</a></li>
@@ -24,7 +23,7 @@
                 </ul>
             </div>
             <div class="col-sm-1 col-lg-1"></div>
-            <div class="col">
+            <div class="col-sm-7 col-lg-5">
                 <h4>Liens</h4>
                 <ul class="u-listUnstyled   ">
                     <li><a href="" class="text-decoration-none text-muted">Conditions d'utilisation</a></li>
@@ -33,12 +32,10 @@
                     <li><a href="" class="text-decoration-none text-muted">Signaler un bug</a></li>
                 </ul>
             </div>
-            <div class="col text-center ">
-                <span class="align-middle">
-                    <img class="mt-3" src="./img/logo.png" width="121" height="36" alt="Logo StageHUB">
-                    <div class="mt-4">Copyright © StageHUB</div>
-                    <div>2018 - 2022</div>
-                </span>
+            <div class="col-sm-24 col-lg-7 text-center">
+                <img class="mt-3" src="./img/logo.png" width="121" height="36" alt="Logo StageHUB">
+                <div class="mt-4">Copyright © StageHUB</div>
+                <div>2018 - 2022</div>
             </div>
         </div>
     </div>
