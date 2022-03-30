a8fbe7e9af181a654f9b1e41e1badee6e7ff6150
Blotzeen <74916752+Blotzeen@users.noreply.github.com>
2022-03-30 17:34:27 +0200

    footer+header

--- www/app/modify_account.php
+++ www/app/modify_account.php
@@ -2,9 +2,9 @@
 session_start();
 require_once('tools/tools.php');
 require_once("tools/bdd.php");
-// require_once('templates/tpl_header.php');
+require_once('templates/tpl_header.php');
 require_once('templates/tpl_modify_account.php');
-// require_once('templates/tpl_footer.php');
+require_once('templates/tpl_footer.php');
 
 $msg = "";
 if (isset($_POST['BtnModAccount'])) {
--- www/app/new_enterprise.php
+++ www/app/new_enterprise.php
@@ -2,7 +2,9 @@
 session_start();
 require_once('tools/tools.php');
 require_once("tools/bdd.php");
+require_once('templates/tpl_header.php');
 require_once('templates/tpl_new_enterprise.php');
+require_once('templates/tpl_footer.php');
 
 $msg = "";
 if (isset($_POST['BtnNewEnterprise'])) {
