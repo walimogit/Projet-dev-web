b2a646e8b49527f0287033d06b80fb8e4291a001
Maxougit <02maxou@gmail.com>
2022-03-30 10:51:15 +0200

    Search V1

--- www/app/search.php
+++ www/app/search.php
@@ -3,11 +3,33 @@ session_start();
 require_once("tools/bdd.php");
 require_once("tools/tools.php");
 
-$search = trim($_GET['search']);
+if(!isLoggedIn())
+    header('Location: login.php');
 
-$result_stage = Search();
+$search = trim($_GET['search']);
+$result_stage = Search2();
+require_once('templates/tpl_header.php');
+
+//  $result = Search();
+
+foreach (Search() as $key) {
+    if ($key['Name_enterprise'] === $search){
+        $result[] = $key['ID_enterprise'] . ' ' . $key['Name_enterprise'] . ' ' . $key['Number_interns_accepted'];
+    }elseif ($key['Competense'] === $search){
+        $result[] = $key['Competense'] . ' ' . $key['Duree_de_stage'] .  ' ' . $key['Base_remuneration'] . ' ' . $key['Date_offre'] . ' ' . $key['Nb_places_offertes'];
+    }elseif ($key[''] === $search){
+        $result[] = '';
+    }elseif ($key[''] === $search){
+        $result[] = '';
+    }    
+}
+if (empty($result)){
+    $result = 'Nothing found';
+}
+
 
 require_once('templates/tpl_search.php');
+require_once('templates/tpl_footer.php');
 
 
 ?>
