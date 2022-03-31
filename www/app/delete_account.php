3d35fb1d669bf2ae87ef93e39ce017cf3a9c4ff2
Maxougit <02maxou@gmail.com>
2022-03-31 16:23:59 +0200

    Verif de perm delete account

--- www/app/delete_account.php
+++ www/app/delete_account.php
@@ -2,6 +2,13 @@
 session_start();
 require_once('tools/tools.php');
 require_once("tools/bdd.php");
+
+if(isStudent() or isEnterprise() or !isDelegateTutor() or !isDelegateAdmin() or !isDelegateStudent()){
+    header('Location: index.php');
+}
+
+$result_stage = Search2();
+
 require_once('templates/tpl_header.php');
 require_once('templates/tpl_delete_account.php');
 require_once('templates/tpl_footer.php');
