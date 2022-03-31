3d21584ae2d383dd26deb4011a11a8f15b0ffbe0
Blotzeen <74916752+Blotzeen@users.noreply.github.com>
2022-03-31 20:34:05 +0200

    v1 upload

--- www/app/templates/tpl_upload.php
+++ www/app/templates/tpl_upload.php
@@ -1,13 +1,21 @@
 <main>
     <section>
-        <div class="row pt-3">
+        <div class="row pt-5">
             <div class="col">
                 <h1 class="lead">Upload your files <i class="fas fa-upload"></i></h1>
-                <form action="upload.php" method="post" enctype="multipart/form-data">
-                    <label for="file">File's name:</label>
-                    <input type="file" class="form-control" name="file" id="file" multiple>
-                    <br />
-                    <input type="submit" name="submit" value="Submit" />
+                <form class="pb-5" action="upload.php" method="post" enctype="multipart/form-data">
+                    <label for="file">Send your CV</label>
+                    <div class="input-group mb-3">
+                        <input type="file" class="form-control" name="file" id="file" />
+                        <button type="submit" name="submit" value="Submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+                <form class="pb-5" action="upload.php" method="post" enctype="multipart/form-data">
+                    <label for="file">Send your CV</label>
+                    <div class="input-group mb-3">
+                        <input type="file" class="form-control" name="file" id="file" />
+                        <button type="submit" name="submit" value="Submit" class="btn btn-primary">Submit</button>
+                    </div>
                 </form>
             </div>
             <div class="col">
@@ -15,10 +23,10 @@
                 <form action="upload.php" method="post" enctype="multipart/form-data">
                     <label for="file">Filename:</label>
                     <input type="file" name="file" id="file" />
-                    <br />
                     <input type="submit" name="submit" value="Submit" />
                 </form>
             </div>
+
         </div>
     </section>
 </main>
