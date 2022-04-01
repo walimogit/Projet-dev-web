87382cdc5b4ac7d5309982bbf192b62ea737663d
JulesROGE <93529053+JulesROGE@users.noreply.github.com>
2022-04-01 08:52:53 +0200

    Update tpl_footer.php

--- www/app/templates/tpl_footer.php
+++ www/app/templates/tpl_footer.php
@@ -1,46 +1,44 @@
 <link rel="stylesheet" href="../css/navbar-top-fixed.css">
 <script src="../vendors/bootstrap/js/bootstrap.js"></script>
-</body>
-<footer>
-    <div class="Footer pb-2 bg-dark">
-        <div class="container">
-            <div class="row mb-3">
-                <div class="col-sm-7 col-lg-5">
-                    <h4>Contact</h4>
-                    <ul class="u-listUnstyled">
-                        <li><a href="mailto:maxence.leroux@viacesi.fr" class="text-decoration-none prin-color">LEROUX MAXENCE</a></li>
-                        <li><a href="mailto:jules.roge@viacesi.fr" class="text-decoration-none prin-color">ROGE JULES</a></li>
-                        <li><a href="mailto:timothee.renaud1@viacesi.fr" class="text-decoration-none prin-color">RENAUD TIMOTHEE</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-1 col-lg-1"></div>
-                <div class="col-sm-7 col-lg-5">
-                    <h4>About</h4>
-                    <ul class="u-listUnstyled">
-                        <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Our Discord</a></li>
-                        <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Our Facebook page</a></li>
-                        <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Our Twitter</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-1 col-lg-1"></div>
-                <div class="col-sm-7 col-lg-5">
-                    <h4>Links</h4>
-                    <ul class="u-listUnstyled">
-                        <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Terms Of Use</a></li>
-                        <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Privacy policy</a></li>
-                        <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Report a Bug</a></li>
-                    </ul>
-                </div>
-                <div class="col text-center">
-                    <img class="mt-3" src="./img/logo.png" width="121" height="36" alt="Logo StageHUB">
-                    <div class="mt-4 sec-color">Copyright &copy; StageHUB</div>
-                    <div class="sec-color">2018 - 2022</div>
-                </div>
+
+<footer class="Footer pb-2 bg-dark">
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col-sm-7 col-lg-5">
+                <h4>Contact</h4>
+                <ul class="u-listUnstyled">
+                    <li><a href="mailto:maxence.leroux@viacesi.fr" class="text-decoration-none prin-color">LEROUX MAXENCE</a></li>
+                    <li><a href="mailto:jules.roge@viacesi.fr" class="text-decoration-none prin-color">ROGE JULES</a></li>
+                    <li><a href="mailto:timothee.renaud1@viacesi.fr" class="text-decoration-none prin-color">RENAUD TIMOTHEE</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-1 col-lg-1"></div>
+            <div class="col-sm-7 col-lg-5">
+                <h4>About</h4>
+                <ul class="u-listUnstyled">
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Our Discord</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Our Facebook page</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Our Twitter</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-1 col-lg-1"></div>
+            <div class="col-sm-7 col-lg-5">
+                <h4>Links</h4>
+                <ul class="u-listUnstyled">
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Terms Of Use</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Privacy policy</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-decoration-none prin-color">Report a Bug</a></li>
+                </ul>
+            </div>
+            <div class="col text-center">
+                <img class="mt-3" src="./img/logo.png" width="121" height="36" alt="Logo StageHUB">
+                <div class="mt-4 sec-color">Copyright &copy; StageHUB</div>
+                <div class="sec-color">2018 - 2022</div>
             </div>
         </div>
     </div>
 </footer>
-
+</body>
 
 
 </html>
